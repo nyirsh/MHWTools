d77690489f3f40f5a55c624cc5148ab23b05b5ee
nyirsh <nyirsh@outlook.com>
2018-08-03 18:41:47 +0200

    Update funcs.js

--- template/funcs.js
+++ template/funcs.js
@@ -30,6 +30,7 @@ function getCurrentLanguage() {
 }
 
 function _gts(section, key) {
+    console.log(section + ' - ' + key);
     var xLang = getCurrentLanguage();
     if (section == 'armor' && key.includes('LV') && key.includes('Slot')) {
         var xString = _gts('armor', 'lvlarmor');
