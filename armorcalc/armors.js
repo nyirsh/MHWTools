eed016f76b17ec585784db7821e9ef2c2e18521c
nyirsh <nyirsh@outlook.com>
2018-08-03 18:33:29 +0200

    Update armors.js

--- armorcalc/armors.js
+++ armorcalc/armors.js
@@ -11648,5 +11648,65 @@ var pa = [
 	name: "Fair Wind Charm",
 	skills: { "Attack Boost": 1, "Divine Blessing": 1 },
 	type: 3
-}
+},
+{
+    maxdef: 92,
+    slotlevels: [3, 1, 0],
+    sex: 3,
+    resist: [-2, -2, 3, -2, 4],
+    part: 0,
+    mindef: 72,
+    slots: 2,
+    name: "Drachen Head Alpha",
+    skills: { "Critical Boost": 1, "Soul of the Dragoon": 1, Airborne: 1 },
+    type: 3
+},
+{
+    maxdef: 92,
+    slotlevels: [3, 0, 0],
+    sex: 3,
+    resist: [-2, -2, 3, -2, 4],
+    part: 1,
+    mindef: 72,
+    slots: 1,
+    name: "Drachen Chest Alpha",
+    skills: {"Critical Eye": 2, "Critical Boost": 1, "Soul of the Dragoon": 1 },
+    type: 3
+},
+{
+    maxdef: 92,
+    slotlevels: [2, 2, 0],
+    sex: 3,
+    resist: [-2, -2, 3, -2, 4],
+    part: 2,
+    mindef: 72,
+    slots: 2,
+    name: "Drachen Arms Alpha",
+    skills: {"Critical Eye": 2, "Attack Boost": 1, "Soul of the Dragoon": 1 },
+    type: 3
+},
+{
+    maxdef: 92,
+    slotlevels: [3, 0, 0],
+    sex: 3,
+    resist: [-2, -2, 3, -2, 4],
+    part: 3,
+    mindef: 72,
+    slots: 1,
+    name: "Drachen Waist Alpha",
+    skills: {"Critical Eye": 2, "Power Prolonger": 1, "Soul of the Dragoon": 1 },
+    type: 3
+},
+{
+    maxdef: 92,
+    slotlevels: [2, 0, 0],
+    sex: 3,
+    resist: [-2, -2, 3, -2, 4],
+    part: 4,
+    mindef: 72,
+    slots: 1,
+    name: "Drachen Legs Alpha",
+    skills: {"Attack Boost": 2, "Critical Boost": 1, "Soul of the Dragoon": 1 },
+    type: 3
+},
 ];
