ddfff9ddd138c0d0d8cc403169637955bbdf5c02
nyirsh <nyirsh@outlook.com>
2018-10-04 09:49:58 +0200

    Update armors.js
    Added Lunastra Gamma

--- armorcalc/armors.js
+++ armorcalc/armors.js
@@ -1779,6 +1779,18 @@ var pa = [
 	type: 3
 },
 {
+	maxdef: 90,
+	slotlevels: [1, 1, 1],
+	sex: 3,
+	resist: [3, 1, 1, -3, -2],
+	part: 0,
+	mindef: 70,
+	slots: 3,
+	name: "Empress Head Gamma",
+	skills: { "Lunastra Favor": 1, "Wide-Range": 2, "Peak Performance": 1 },
+	type: 3
+},
+{
 	maxdef: 84,
 	slotlevels: [2, 1, 0],
 	sex: 3,
@@ -3605,6 +3617,18 @@ var pa = [
 	type: 3
 },
 {
+	maxdef: 90,
+	slotlevels: [3, 0, 0],
+	sex: 3,
+	resist: [3, 1, 1, -3, -2],
+	part: 1,
+	mindef: 70,
+	slots: 1,
+	name: "Empress Chest Gamma",
+	skills: { "Earplugs": 2, "Tool Specialist": 1, "Lunastra Favor": 1 },
+	type: 3
+},
+{
 	maxdef: 72,
 	slotlevels: [0, 0, 0],
 	sex: 3,
@@ -5362,6 +5386,18 @@ var pa = [
 	type: 3
 },
 {
+	maxdef: 90,
+	slotlevels: [3, 1, 0],
+	sex: 3,
+	resist: [3, 1, 1, -3, -2],
+	part: 2,
+	mindef: 70,
+	slots: 2,
+	name: "Empress Arms Gamma",
+	skills: { "Agitator": 2, "Evade Extender": 1, "Lunastra Favor": 1 },
+	type: 3
+},
+{
 	maxdef: 72,
 	slotlevels: [0, 0, 0],
 	sex: 3,
@@ -7067,6 +7103,18 @@ var pa = [
 	slots: 2,
 	name: "Empress Waist Beta",
 	skills: { "Lunastra Favor": 1, "Tool Specialist": 2 },
+	type: 3
+},
+{
+	maxdef: 90,
+	slotlevels: [3, 0, 0],
+	sex: 3,
+	resist: [3, 1, 1, -3, -2],
+	part: 3,
+	mindef: 70,
+	slots: 1,
+	name: "Empress Waist Gamma",
+	skills: { "Lunastra Favor": 1, "Evade Window": 2, "Health Boost": 2 },
 	type: 3
 },
 {
@@ -8800,6 +8848,18 @@ var pa = [
 	slots: 2,
 	name: "Empress Legs Beta",
 	skills: { "Lunastra Favor": 1, "Health Boost": 2 },
+	type: 3
+},
+{
+	maxdef: 90,
+	slotlevels: [3, 0, 0],
+	sex: 3,
+	resist: [3, 1, 1, -3, -2],
+	part: 4,
+	mindef: 70,
+	slots: 1,
+	name: "Empress Legs Gamma",
+	skills: { "Lunastra Favor": 1, "Earplugs": 2, "Agitator": 1 },
 	type: 3
 },
 {
