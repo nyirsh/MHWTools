d09789091da2221c63f6f44a4257f1c17f4d6c17
Davide Villa <nyirsh@outlook.com>
2019-05-10 12:32:36 +0200

    Added Nergi Gamma

--- armorcalc/armors.js
+++ armorcalc/armors.js
@@ -12669,4 +12669,64 @@ var pa = [
     skills: {"Divine Blessing": 1, "Astera Blessing": 1},
     type: 3
 },
+	{
+	maxdef: 92,
+	slotlevels: [2, 2, 0],
+	sex: 3,
+	resist: [1, 1, -3, 1, -3],
+	part: 0,
+	mindef: 72,
+	slots: 2,
+	name: "Nergigante Head Gamma",
+	skills: { "Nergigante Hunger": 1, "Stamina Surge": 2, "Agitator": 1 },
+	type: 3
+},
+{
+	maxdef: 92,
+	slotlevels: [3, 2, 1],
+	sex: 3,
+	resist: [1, 1, -3, 1, -3],
+	part: 1,
+	mindef: 72,
+	slots: 3,
+	name: "Nergigante Chest Gamma",
+	skills: { "Nergigante Hunger": 1, "Maximum Might": 1 },
+	type: 3
+},
+{
+	maxdef: 92,
+	slotlevels: [2, 1, 1],
+	sex: 3,
+	resist: [1, 1, -3, 1, -3],
+	part: 2,
+	mindef: 72,
+	slots: 3,
+	name: "Nergigante Arms Gamma",
+	skills: { "Nergigante Hunger": 1, "Partbreaker": 1, "Attack Boost": 1 },
+	type: 3
+},
+{
+	maxdef: 92,
+	slotlevels: [3, 1, 0],
+	sex: 3,
+	resist: [1, 1, -3, 1, -3],
+	part: 3,
+	mindef: 72,
+	slots: 2,
+	name: "Nergigante Waist Gamma",
+	skills: { "Nergigante Hunger": 1, "Maximum Might": 1, "Agitator": 1 },
+	type: 3
+},
+{
+	maxdef: 92,
+	slotlevels: [2, 2, 0],
+	sex: 3,
+	resist: [1, 1, -3, 1, -3],
+	part: 4,
+	mindef: 72,
+	slots: 2,
+	name: "Nergigante Legs Gamma",
+	skills: { "Nergigante Hunger": 1, "Attack Boost": 3 },
+	type: 3
+},
 ];
