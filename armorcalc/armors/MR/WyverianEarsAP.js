68861daaf7f5b531351428c4ba3e29e3e8511bf8
Maximilien Rigaut <max.rigaut@gmail.com>
2020-01-30 11:51:02 +0100

    Update WyverianEarsAP.js

--- armorcalc/armors/MR/WyverianEarsAP.js
+++ armorcalc/armors/MR/WyverianEarsAP.js
@@ -1,3 +1,14 @@
 [
-
+    {
+        maxdef: 140,
+        slotlevels: [4, 0, 0],
+        sex: 3,
+        resist: [-3, 2, -2, 3, 2],
+        part: 0,
+        mindef: 140,
+        slots: 0,
+        name: "Wyverian Ears Alpha +",
+        skills: { "Mushroomancer": 2,"Survival Expert": 1 },
+        type: 3
+    }
 ]
